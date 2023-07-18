154098bab32c1c8c22cb3bf6299d80f6c365bd40
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-07-18 23:16:57 +0200

    Delete script.sh

--- app/data/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Importiere Daten in die MongoDB-Datenbank sammlung
-# Verwende das JSON-Array-Format für die Importdatei /data/data.json
-mongoimport --jsonArray --db pokedex --collection pokemon --file /data/data.json
